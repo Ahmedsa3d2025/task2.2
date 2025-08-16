d3d858f51c14fd294583c65ddc7dc22885d4a913
Ahmedsa3d2025 <ahmedsa3d97.2020@gmail.com>
2025-08-16 16:11:06 +0300

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<string>
+using namespace std;
+#define ll long long
+#define en endl
+
+int main()
+{
+    int y;
+    cout << "pleese enter y:" ;
+    cin >> y;
+    y += 5;
+    cout << 'y' << '=' << y << en;
+    y -= 2;
+    cout << 'y' << '=' << y << en;
+    y *= 3;
+    cout << 'y' << '=' << y << en;
+    y /= 2;
+    cout << 'y' << '=' << y << en;
+
+
+   
+return 0;
+}
+       
